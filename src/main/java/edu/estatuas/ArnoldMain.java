d9f2ac3faaa8c523586fc7fba51bc80b34274cf1
Desteveco <destevezcounago@gmail.com>
2025-03-31 15:08:11 +0200

    style: upgrade prints

--- src/main/java/edu/estatuas/ArnoldMain.java
+++ src/main/java/edu/estatuas/ArnoldMain.java
@@ -8,18 +8,18 @@ import edu.estatuas.logica.Planeta;
 public class ArnoldMain{
 
     public static void main(String[] args) {
-        double peso = 1.0; //Kg
+        double peso = 73.0; //Kg
 
         for (Planeta planeta : Planeta.values()) {
-            System.out.printf("Your weight on %s is %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
+            System.out.printf("Your weight on %s is %f N%n\n", planeta.name(), planeta.pesoSuperficie(peso));
         }
 
-        System.out.println("Your weight on the terrestrial planets:");
+        System.out.println("Your weight on the terrestrial planets:\n");
         for (Planeta planeta : Planeta.getPlanetasTerrestres()) {
             System.out.printf("Your weight on %s is %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
         }
 
-        System.out.println("Your weight on the gas giant planets:");
+        System.out.println("Your weight on the gas giant planets:\n");
         for (Planeta planeta : Planeta.getGigantesGaseosos()) {
             System.out.printf("Your weight on %s is %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
         }
